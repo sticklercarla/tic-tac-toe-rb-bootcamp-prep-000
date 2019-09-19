1c3f48ef72e5448221ec819bf8080bbb5151affe
Carla Stickler <carlastickler@gmail.com>
2019-09-19 00:10:40 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,3 +50,12 @@ def turn_count(board)
   board.count { |token| token == 'X' || token == 'O' }
 end
 
+def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+      board[combo[1]] == board[combo[2]] &&
+      position_taken?(board, combo[0])
+  end
+end
+
+
